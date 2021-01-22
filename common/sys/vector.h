419b71048dd73ab380d1405de9f399a7228740b1
Michael Tao <mtao@dgp.toronto.edu>
2021-01-21 22:18:34 -0500

    using c++11 allocator_traits to destroy with the allocator, which is compatible with c++20

--- common/sys/vector.h
+++ common/sys/vector.h
@@ -5,6 +5,7 @@
 
 #include "alloc.h"
 #include <algorithm>
+#include <memory>
 
 namespace embree
 {
@@ -127,14 +128,14 @@ namespace embree
       {
         assert(!empty());
         size_active--;
-        alloc.destroy(&items[size_active]);
+        std::allocator_traits<allocator>::destroy(alloc,&items[size_active]);
       }
 
       __forceinline void clear() 
       {
         /* destroy elements */
         for (size_t i=0; i<size_active; i++)
-          alloc.destroy(&items[i]);
+          std::allocator_traits<allocator>::destroy(alloc,&items[i]);
         
         /* free memory */
         alloc.deallocate(items,size_alloced); 
@@ -179,7 +180,7 @@ namespace embree
         if (new_active < size_active) 
         {
           for (size_t i=new_active; i<size_active; i++)
-            alloc.destroy(&items[i]);
+            std::allocator_traits<allocator>::destroy(alloc,&items[i]);
           size_active = new_active;
         }
 
@@ -195,7 +196,7 @@ namespace embree
         items = alloc.allocate(new_alloced);
         for (size_t i=0; i<size_active; i++) {
           ::new (&items[i]) T(std::move(old_items[i]));
-          alloc.destroy(&old_items[i]);
+          std::allocator_traits<allocator>::destroy(alloc,&old_items[i]);
         }
 
         for (size_t i=size_active; i<new_active; i++) {
